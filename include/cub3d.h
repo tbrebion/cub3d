e1593be4866d714a7f290db934b1ea2d6d373b92
Titouan BREBION <tbrebion@e1r4p17.clusters.42paris.fr>
2022-08-01 10:49:02 +0200

    wip

---
+++ include/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 10:43:18 by tbrebion          #+#    #+#             */
+/*   Updated: 2022/08/01 10:48:04 by tbrebion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../minilibx-linux/mlx.h"
+
+#endif
