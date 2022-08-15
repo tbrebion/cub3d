22a1abb7ffb8b0f61141887e7951801b6824989b
tbrebion <tbrebion@student.42.fr>
2022-08-15 18:49:58 +0200

    wip

--- src/render/render.c
+++ src/render/render.c
@@ -6,26 +6,28 @@
 /*   By: tbrebion <tbrebion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 15:27:45 by tbrebion          #+#    #+#             */
-/*   Updated: 2022/08/10 15:47:56 by tbrebion         ###   ########.fr       */
+/*   Updated: 2022/08/15 18:46:19 by tbrebion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static int	key_press(int keysym);
+int	key_press(int keysym);
 
 void	init_mlx(void)
 {
 	g_data.game.mlx = mlx_init();
 	garcol_add(g_data.game.mlx);
 	g_data.game.win = mlx_new_window(g_data.game.mlx, 900, 600, "./cub3d");
+	g_data.game.img = mlx_new_image(g_data.game.mlx, 900, 600);
 	mlx_key_hook(g_data.game.win, &key_press, &g_data);
 }
 
-static int	key_press(int keysym)
+int	key_press(int keysym)
 {
-	if (keysym == KEY_ESC)
+	if (keysym == 113)
 	{
+		// ft_putstr_fd("\nOKOKOKOK\n", 0);
 		mlx_clear_window(g_data.game.mlx, g_data.game.win);
 		mlx_destroy_window(g_data.game.mlx, g_data.game.win);
 		garcol_free_all();
